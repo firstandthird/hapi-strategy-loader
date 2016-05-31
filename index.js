a52ec5e5860d7b328d03efef6037371f1dfcadd4
Chris <orthagonal1@gmail.com>
2016-05-31 16:00:42 -0500

    checks that plugins are able to use profile function

--- index.js
+++ index.js
@@ -2,25 +2,15 @@
 const _ = require('lodash');
 
 exports.register = (server, config, next) => {
-  // set up logging:
-  const log = (tags, content) => {
-    if (!config.verbose) {
-      return;
-    }
-    server.log(tags, content);
-  };
-  log(['hapi-strategy-loader', { msg: 'Loading strategy plugins' }]);
   _.forIn(config.strategies, (value, name) => {
-    log(['hapi-strategy-loader'], { message: 'strategy loaded', strategy: name, options: value });
-    const profileFn = _.get(value, 'options.provider.profile');
-    if (typeof profileFn === 'string') {
-      value.options.provider.profile = (credentials, params, get, callback) => {
-        server.methods[profileFn](credentials, params, get, callback);
-      };
-    }
-    server.auth.strategy(name, value.scheme, value.mode, value.options);
+   const profileFn = _.get(value, 'options.provider.profile');
+   if (typeof profileFn === 'string') {
+     value.options.provider.profile = (credentials, params, get, callback) => {
+       server.methods[profileFn](credentials, params, get, callback);
+     };
+   }
+   server.auth.strategy(name, value.scheme, value.mode, value.options);
   });
-  log(['hapi-strategy-loader'], { msg: 'Strategies and plugins loaded' });
   next();
 };
 
---
+++ test/bell-test.js
@@ -0,0 +1,95 @@
+// verifies that npm package 'bell' will load correctly as a test case:
+'use strict';
+
+const Hapi = require('hapi');
+const code = require('code');
+const lab = exports.lab = require('lab').script();
+const bell = require('bell');
+const strategyLoader = require('../');
+
+const password = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnop';
+const config = {
+  strategies: {
+    twitter: {
+      scheme: 'bell',
+      mode: 'try',
+      options: {
+        password,
+        provider: 'google',
+        clientId: 'pretend_twitter_id',
+        clientSecret: 'pretend_secret_id',
+        isSecure: false
+      }
+    }
+  }
+};
+
+let server;
+lab.beforeEach((done) => {
+  server = new Hapi.Server({ });
+  server.connection({ port: 8080 });
+  server.register(bell, () => {
+    server.methods.profileFunc = () => {
+      console.log('profileFunc is called');
+    }
+    done();
+  });
+});
+
+lab.afterEach((done) => {
+  server.stop(() => {
+    done();
+  });
+});
+
+lab.test('bell is configured with profile func', (done) => {
+  server.register({
+    register: strategyLoader,
+    options: config
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    server.start(() => {
+      let success = false;
+      try {
+        server.auth.strategy('twitter', 'bell', 'try', {});
+      } catch (e) {
+        code.expect(e.toString()).to.equal('Error: Authentication strategy name already exists');
+        success = true;
+      }
+      code.expect(success).to.equal(true);
+      done();
+    });
+  });
+});
+
+lab.test('bell strategy prevents access of protected routes ', (done) => {
+  server.register({
+    register: strategyLoader,
+    options: config
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    server.route({
+      method: 'GET',
+      path: '/',
+      config: {
+        auth: 'twitter',
+        handler: (request, reply) => {
+          reply('hello!');
+        }
+      }
+    });
+    server.start(() => {
+      server.inject({
+        url: '/',
+      }, (res) => {
+        // bell will redirect us on auth failure:
+        code.expect(res.statusCode).to.equal(302);
+        done();
+      });
+    });
+  });
+});
