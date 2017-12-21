a08df4ab3af42d932378719715e85d313c92c3f9
Chris <orthagonal1@gmail.com>
2017-12-21 14:34:13 -0600

    return falue from validateFunc

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const register = (server, config) => {
     }
     const profileFn = _.get(value, 'options.provider.profile');
     if (typeof profileFn === 'string') {
-      value.options.provider.profile = async(credentials, params, get) => {
+      value.options.provider.profile = (credentials, params, get) => {
         const method = _.get(server.methods, profileFn);
         if (!method) {
           const msg = `could not find profile method ${profileFn} in server.methods`;
           server.log(['error', 'hapi-strategy-loader'], msg);
           return;
         }
-        await method(credentials, params, get);
+        return method(credentials, params, get);
       };
     }
     const validateFn = _.get(value, 'options.validateFunc');
     if (typeof validateFn === 'string') {
-      value.options.validateFunc = async(request, session) => {
+      value.options.validateFunc = (request, session) => {
         const method = _.get(server.methods, validateFn);
         if (!method) {
           const msg = `could not find validate method ${validateFn} in server.methods`;
           server.log(['error', 'hapi-strategy-loader'], msg);
           return;
         }
-        await method(request, session);
+        return method(request, session);
       };
     }
     server.auth.strategy(name, value.scheme, value.options);
