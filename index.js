50698333f62aea77541e072751d6b1d506d1cc3b
Chris <orthagonal1@gmail.com>
2017-12-21 14:30:21 -0600

    update tests

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 'use strict';
 const _ = require('lodash');
 
-exports.register = (server, config, next) => {
+const register = (server, config) => {
   _.forIn(config.strategies, (value, name) => {
     if (config.verbose) {
       server.log(['hapi-strategy-loader'], { message: 'strategy loaded', strategy: name, options: value });
     }
     const profileFn = _.get(value, 'options.provider.profile');
     if (typeof profileFn === 'string') {
-      value.options.provider.profile = (credentials, params, get, callback) => {
+      value.options.provider.profile = async(credentials, params, get) => {
         const method = _.get(server.methods, profileFn);
         if (!method) {
           const msg = `could not find profile method ${profileFn} in server.methods`;
           server.log(['error', 'hapi-strategy-loader'], msg);
           return;
         }
-        method(credentials, params, get, callback);
+        await method(credentials, params, get);
       };
     }
     const validateFn = _.get(value, 'options.validateFunc');
     if (typeof validateFn === 'string') {
-      value.options.validateFunc = (request, session, callback) => {
+      value.options.validateFunc = async(request, session) => {
         const method = _.get(server.methods, validateFn);
         if (!method) {
           const msg = `could not find validate method ${validateFn} in server.methods`;
           server.log(['error', 'hapi-strategy-loader'], msg);
           return;
         }
-        method(request, session, callback);
+        await method(request, session);
       };
     }
-    server.auth.strategy(name, value.scheme, value.mode, value.options);
+    server.auth.strategy(name, value.scheme, value.options);
+    // the last-registered strategy will be the default:
+    server.auth.default(name);
   });
-  next();
 };
 
-exports.register.attributes = {
+exports.plugin = {
+  name: 'hapi-strategy-loader',
+  register,
+  once: true,
   pkg: require('./package.json')
 };
